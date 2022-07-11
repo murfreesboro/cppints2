9089b352f40b1e783b829a58291691a437d10e88
Fenglai Liu <fhilosophierr@gmail.com>
2022-07-11 12:54:32 -0500

    checkout fmt_test_up.cpp

--- util/fmt_test/fmt_test_up.cpp
+++ util/fmt_test/fmt_test_up.cpp
@@ -18,6 +18,10 @@
 // it introduces greater error compared with other methods. 
 // Therefore we finally abadon it.
 //
+// july 2022:
+// double check the implementation and everything good, revise the fmt function
+// and use long double instead.
+//
 //
 //
 
@@ -33,11 +37,6 @@
 #include<fstream>
 #include<iomanip>
 #include<string> 
-#include<vector>
-#include<list>
-#include<set>
-#include<map>
-#include<iterator>
 #include<algorithm>
 
 #include <boost/math/special_functions/binomial.hpp>  // special functions in math
@@ -49,26 +48,17 @@ using namespace boost;
 using namespace std;
 #define PI              3.1415926535897932384626E0  
 
-double fm(const double& a, const int& m); 
-double doubleFac(const int& m, const int& n); 
+long double fm(const long double& a, const int& m) {
 
-double fm(const double& a, const int& m) {
-	double x = 1.0E0;
-	if (fabs(a)<1.0E-16) return (pow(x,2*m+1)/(2*m+1));
-	double f12 = 1.0E0/2.0E0;
-	double p   = 1.0E0/(2.0E0*pow(a,m+f12));
-	double upperLimit = a*pow(x,2.0E0);
+	// if a is very small, the e^{-ax^2} term turns to zero
+	if (fabs(a)<1.0E-20) return (1.0E0/(2*m+1));
+
+	// this is the way we use incomplete gamma in boost
+	long double f12 = 1.0E0/2.0E0;
+	long double p   = 1.0E0/(2.0E0*pow(a,m+f12));
+	long double upperLimit = a;
 	return tgamma_lower(m+f12,upperLimit)*p;
 }
-
-double doubleFac(const int& m, const int& n) {
-	// calculate (2m-1)!!/(2m+2n+1)!!
-	double x = 1.0E0;
-	for(int i=0; i<=n; i++) {
-		x *= 1.0E0/(2*m+2*i+1); 
-	}
-	return x;
-} 
 
 int main() 
 {
@@ -80,16 +70,16 @@ int main()
 	// if the difference is less than the error, we 
 	// think the error could be omitted
 	///////////////////////////////////////////////////
-	double steplength  = 0.0000001;
-	double globalError = 1.0E-16;
-	int M_limmit       = 8;
-	double T_max_limit = 10.0E0;
-	double T_min_limit = 1.6E0;
-	int numSteps       = (T_max_limit-T_min_limit)/steplength;
-	double largest     = 0.0E0;
+	long double steplength  = 0.00000001;
+	long double globalError = 1.0E-16;
+	int M_limmit            = 4;
+	long double T_max_limit = 50.0E0;
+	long double T_min_limit = 1.39E0;
+	size_t numSteps         = (size_t)((T_max_limit-T_min_limit)/steplength);
+	long double largest     = 0.0E0;
 
 	/////////////////////////////////////////////////////////////////////
-	// this section testing the up recursice relation for M_limit = 8  //
+	// this section testing the up recursice relation for M_limit 
 	/////////////////////////////////////////////////////////////////////
 	cout << endl << endl;
 	cout << "===========================================================" << endl;
@@ -98,92 +88,26 @@ int main()
 	cout << "step length for T is " << steplength << endl;
 	cout << "T is ranging from " << T_min_limit << " to " <<T_max_limit << endl;
 	cout << "===========================================================" << endl;
-	for (int j = 0; j<numSteps; j++) {
-		double T = T_min_limit+steplength*j;
-		double sqT = sqrt(T);
-		double fmt= erf(sqT)*sqrt(PI)/2.0E0;
-		double et = exp(-T);
-		double e0 = fmt/sqT;
-		double oneO2T = 0.5E0/T;
+	for (size_t j = 0; j<numSteps; j++) {
+		long double T = T_min_limit+steplength*j;
+		long double sqT = sqrt(T);
+		long double fmt= erf(sqT)*sqrt(PI)/2.0E0;
+		long double et = exp(-T);
+		long double e0 = fmt/sqT;
+		long double oneO2T = 0.5E0/T;
 		for(int i=1; i<=M_limmit; i++) {
-			double s = fm(T,i);
-			double e = oneO2T*((2*i-1)*e0-et);
-			double d  = fabs(s-e);
-			double d0 = s-e;
+			long double s = fm(T,i);
+			long double e = oneO2T*((2*i-1)*e0-et);
+			long double d  = fabs(s-e);
+			long double d0 = s-e;
 			if (d>globalError) {
-				printf("m is  %d, T   is %-18.16f, difference is %-18.16f\n", i, T, d0);
+				printf("m is  %d, T   is %-18.16Lf, difference is %-18.16Lf\n", i, T, d0);
 				if (d>largest) largest = d;
 			}
 			e0 = e;
 		}
 	}
-	printf("largest difference is %-16.14f\n", largest);
-
-	/////////////////////////////////////////////////////////////////////
-	// this section testing the up recursice relation for M_limit = 9  //
-	/////////////////////////////////////////////////////////////////////
-	cout << endl << endl;
-	M_limmit    = 9;
-	cout << "===========================================================" << endl;
-	cout << "testing the up recursive relation for m = 1 to " << M_limmit << endl;
-	cout << "error range is " << globalError << endl;
-	cout << "step length for T is " << steplength << endl;
-	cout << "T is ranging from " << T_min_limit << " to " <<T_max_limit << endl;
-	cout << "===========================================================" << endl;
-	largest = 0.0E0;
-	for (int j = 0; j<numSteps; j++) {
-		double T = T_min_limit+steplength*j;
-		double sqT = sqrt(T);
-		double fmt= erf(sqT)*sqrt(PI)/2.0E0;
-		double et = exp(-T);
-		double e0 = fmt/sqT;
-		double oneO2T = 0.5E0/T;
-		for(int i=1; i<=M_limmit; i++) {
-			double s = fm(T,i);
-			double e = oneO2T*((2*i-1)*e0-et);
-			double d  = fabs(s-e);
-			double d0 = s-e;
-			if (d>globalError) {
-				printf("m is  %d, T   is %-18.16f, difference is %-18.16f\n", i, T, d0);
-				if (d>largest) largest = d;
-			}
-			e0 = e;
-		}
-	}
-	printf("largest difference is %-16.14f\n", largest);
-
-	/////////////////////////////////////////////////////////////////////
-	// this section testing the up recursice relation for M_limit = 10 //
-	/////////////////////////////////////////////////////////////////////
-	cout << endl << endl;
-	M_limmit    = 10;
-	cout << "===========================================================" << endl;
-	cout << "testing the up recursive relation for m = 1 to " << M_limmit << endl;
-	cout << "error range is " << globalError << endl;
-	cout << "step length for T is " << steplength << endl;
-	cout << "T is ranging from " << T_min_limit << " to " <<T_max_limit << endl;
-	cout << "===========================================================" << endl;
-	largest = 0.0E0;
-	for (int j = 0; j<numSteps; j++) {
-		double T = T_min_limit+steplength*j;
-		double sqT = sqrt(T);
-		double fmt= erf(sqT)*sqrt(PI)/2.0E0;
-		double et = exp(-T);
-		double e0 = fmt/sqT;
-		double oneO2T = 0.5E0/T;
-		for(int i=1; i<=M_limmit; i++) {
-			double s = fm(T,i);
-			double e = oneO2T*((2*i-1)*e0-et);
-			double d  = fabs(s-e);
-			double d0 = s-e;
-			if (d>globalError) {
-				printf("m is  %d, T   is %-18.16f, difference is %-18.16f\n", i, T, d0);
-				if (d>largest) largest = d;
-			}
-			e0 = e;
-		}
-	}
-	printf("largest difference is %-16.14f\n", largest);
+	printf("largest difference is %-18.16Lf\n", largest);
 
 	return 0;
 }
