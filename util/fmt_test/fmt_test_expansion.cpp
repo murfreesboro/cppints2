67c1940053f873acb16f77d56b99a7a0f9f89281
Fenglai Liu <fhilosophierr@gmail.com>
2022-07-11 14:40:59 -0500

    update fmt_test_expansion

--- util/fmt_test/fmt_test_expansion.cpp
+++ util/fmt_test/fmt_test_expansion.cpp
@@ -18,6 +18,9 @@
 // it introduces greater error compared with other methods. 
 // Therefore we finally abadon it.
 //
+// july 2022:
+// double check the implementation and everything good, revise the fmt function
+// and use long double instead.
 //
 //
 
@@ -31,13 +34,8 @@
 // common C++ head files may be used in the program
 #include<iostream>
 #include<fstream>
-#include<iomanip>
 #include<string> 
 #include<vector>
-#include<list>
-#include<set>
-#include<map>
-#include<iterator>
 #include<algorithm>
 
 #include <boost/math/special_functions/binomial.hpp>  // special functions in math
@@ -49,26 +47,17 @@ using namespace boost;
 using namespace std;
 #define PI              3.1415926535897932384626E0  
 
-double fm(const double& a, const int& m); 
-double doubleFac(const int& m, const int& n); 
+long double fm(const long double& a, const int& m) {
 
-double fm(const double& a, const int& m) {
-	double x = 1.0E0;
-	if (fabs(a)<1.0E-16) return (pow(x,2*m+1)/(2*m+1));
-	double f12 = 1.0E0/2.0E0;
-	double p   = 1.0E0/(2.0E0*pow(a,m+f12));
-	double upperLimit = a*pow(x,2.0E0);
+	// if a is very small, the e^{-ax^2} term turns to zero
+	if (fabs(a)<1.0E-20) return (1.0E0/(2*m+1));
+
+	// this is the way we use incomplete gamma in boost
+	long double f12 = 1.0E0/2.0E0;
+	long double p   = 1.0E0/(2.0E0*pow(a,m+f12));
+	long double upperLimit = a;
 	return tgamma_lower(m+f12,upperLimit)*p;
 }
-
-double doubleFac(const int& m, const int& n) {
-	// calculate (2m-1)!!/(2m+2n+1)!!
-	double x = 1.0E0;
-	for(int i=0; i<=n; i++) {
-		x *= 1.0E0/(2*m+2*i+1); 
-	}
-	return x;
-} 
 
 int main() 
 {
@@ -80,45 +69,46 @@ int main()
 	// if the difference is less than the error, we 
 	// think the error could be omitted
 	///////////////////////////////////////////////////
-	int N = 10000000;
-	double steplength  = 0.0000001;
-	double globalError = 1.0E-16;
-	int M_limmit       = 40;
-	double T_min_limit = 1.6E0;
+	long double steplength  = 0.0000001;
+	long double globalError = 1.0E-16;
+	int M_limmit = 4;
+	int M_lower_limit = 1;
+	long double T_min_limit = 1.4E0;
    int nTerms = 17;
+
+	// testing the accurary of long double
+	printf("sizeof long double: %d\n", sizeof(long double));
 
 	///////////////////////////////////////////////////
 	// testing the power series
 	// equation of 9 in Harris paper
 	// We try to expand the power series 
-	// here we test from m=1 to m=10
-	// for T from 0 to 1.4
 	///////////////////////////////////////////////////
-	int numSteps = T_min_limit/steplength;
+	size_t numSteps = T_min_limit/steplength;
 	cout << endl << endl;
 	cout << "===========================================================" << endl;
 	cout << "testing the power series expansion with " << nTerms+1 << " terms" << endl;
-	cout << "this is for double type variable" << endl;
+	cout << "this is for long double type variable" << endl;
 	cout << "error range is " << globalError << endl;
 	cout << "step length for T is " << steplength << endl;
 	cout << "T is ranging from 0 to " << T_min_limit << endl;
+	cout << "m is from "  << M_lower_limit << " to " << M_limmit << endl;
 	cout << "===========================================================" << endl;
-	for (int m = 1; m<=M_limmit; m++) {
-		for (int j = 0; j<=numSteps; j++) {
-			double T = steplength*j;
-			double et = exp(-T);
-			double x  = 1.0;
-			double t2 = 2.0*T;
+	for (int m = M_lower_limit; m<=M_limmit; m++) {
+		for (size_t j = 0; j<=numSteps; j++) {
+			long double T = 0.0E0 + steplength*j;
+			long double et = exp(-T);
+			long double x  = 1.0;
+			long double t2 = 2.0*T;
 			for(int i=nTerms; i>=1; i--) {
 				x = 1.0+t2/(2.0*(m+i)+1.0)*x;
 			}
 			x  = (1.0/(2*m+1))*x;
 			x *= et;
-			double result = fm(T,m);
-			double d = fabs(result-x);
+			long double result = fm(T,m);
+			long double d = fabs(result-x);
 			if (d>globalError) {
-				printf("fmt is %-18.16f\n", result);
-				printf("for m=%d T=%f difference is %-18.16f\n", m, T, d);
+				printf("fmt is %-18.16Lf, for m=%d T=%-18.16Lf difference is %-18.16Lf\n", result, m, T, d);
 			}
 		}
 	}
